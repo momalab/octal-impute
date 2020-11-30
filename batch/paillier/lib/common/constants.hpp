e5af04ecdbc7f4588f732839db1aa9fcb76472c0
Edu <25762736+educhielle@users.noreply.github.com>
2020-11-30 17:01:56 -0300

    shift-1

--- batch/paillier/lib/common/constants.hpp
+++ batch/paillier/lib/common/constants.hpp
@@ -10,6 +10,6 @@ namespace constants
     const string filePublicKey = "public.key";
     const uint64_t margin = 64; // bits
     const uint64_t bitPrecision = 8;
-    const uint64_t scale = 1<<bitPrecision; //10000; // bits
+    const uint64_t scale = 1<<bitPrecision-1; //10000; // bits
     const uint64_t us = 1000000;
 }
