fa1cb2ac45ecf2cad0daa6d8bec179d50b3609eb
Edu <25762736+educhielle@users.noreply.github.com>
2020-12-01 13:29:57 -0300

    dependencies

---
+++ batch/install_dependencies.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y gcc, g++
+sudo apt install -y python3, python3-pip
+pip3 install --upgrade numpy==1.18.5 pandas==1.1.2 tensorflow==2.3.0
+pip3 install sklearn
+pip3 install matplotlib
